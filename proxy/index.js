5bc3a76f0915a1b17c1a7dd355402ac20a1ca6bd
Mladen Milankovic <developer@mmlado.com>
2023-08-29 19:59:38 +0200

    Add proxy server

--- proxy/index.js
+++ proxy/index.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const cors = require('cors')
-const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
 app.use(cors())
-app.use(createProxyMiddleware({
-  router: (req) => new URL(req.path.substring(1)),
-  pathRewrite: (path, req) => {
-    const url = new URL(req.originalUrl.substring(1));
-    return url.pathname + url.search;
-  },
-  changeOrigin: true,
-  logger: console
-}))
+
+const sites = {
+  'liberlandorg': 'https://api.liberland.org/news?lang=en&page=1&limit=2&order=-published',
+}
+
+app.get('/:site', async (req, resp) => {
+  await fetch(sites[req.params.site])
+    .then((data) => data.json())
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => console.log(err));
+});
 
 app.listen(8088, () => {
   console.info('proxy server is running on port 8088')
