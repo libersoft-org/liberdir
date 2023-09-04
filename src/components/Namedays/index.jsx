f377959e9833ef2eb2e61dc0ac849164e47fc119
Mladen Milankovic <developer@mmlado.com>
2023-09-04 21:03:36 +0200

    Refactoring

--- src/components/Namedays/index.jsx
+++ src/components/Namedays/index.jsx
@@ -1,22 +1,23 @@
 import chech from './chech.json';
 
 const chech_lookup = (date) => {
-    console.log(date);
-    return chech.find((element) => element.date > date);
+    return chech.find((element) => element.date > date).name;
 };
 
 const Namedays = () => {
     const today = new Date();
     const tomorrow = new Date(today);
-    tomorrow.setDate(today.getDate() + 1); 
+    tomorrow.setDate(today.getDate() + 1);
+    const dates = [
+        today.toISOString().slice(0, 10),
+        tomorrow.toISOString().slice(0, 10)
+    ]; 
+    
     const names = {
-        'Chech': {
-            'today': chech_lookup(today.toISOString().slice(0, 10)).name,
-            'tomorrow': chech_lookup(tomorrow.toISOString().slice(0, 10)).name
-        }
+        'Chech': dates.map(date => chech_lookup(date))
     };
 
-    const items = Object.keys(names).map((key, index) => <div key={index}><div>{key}</div>{Object.values(names[key]).map((value, idx) => <div key={idx}>{value}</div>)}</div>)
+    const items = Object.keys(names).map((key, index) => <div key={index}><div>{key}</div>{names[key].map((value, idx) => <div key={idx}>{value}</div>)}</div>)
     return <div id='namedays'>
         <div className='title'>Namedays</div>
         <div>
