73735dae9be9ec25c9fe425b839ab3402c160c18
mligor <1839481+mligor@users.noreply.github.com>
2023-09-13 22:49:56 +0200

    #10 finished weather forecast and current weather component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import News from './components/news/news';
 import Weather from './components/Weather/Weather';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <div id='header'>
         <div className='inside'>
           <a className='logo'>LiberDir</a>
+          <span className='spacer'></span>
+          <div className='weather'>
+            <Weather current/>
+          </div>
         </div>
       </div>
       <div id='content'>
--- src/components/Weather/ForecastTile.jsx
+++ src/components/Weather/ForecastTile.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useLocalStorage } from "@uidotdev/usehooks";
+
 const ForecastTile = ({forecast}) => {
+
+  const defaultTemperatureUnit = navigator.language === 'en-US' ? 'f' : 'c';
+
+  const [temperatureUnit, setTemperatureUnit] = useLocalStorage("temperature-unit", defaultTemperatureUnit);
+
+  function changeUnit(unit) {
+    setTemperatureUnit(unit)
+  }
 
   return <div className='weater-tile'>
     <div className='weater-info'>
-      <img src={forecast.image} className='weater-icon' alt={forecast.description} title={forecast.description}></img>
-      <div className='weater-temperatures'>
-        <div className='weater-temperature'>
-          <div className='weater-temperature-max'>{ forecast.temp_max.toFixed(0)}</div>
-          <div className='weater-temperature-min'>{forecast.temp_min.toFixed(0)}</div>
-        </div>
-        <div className='weater-temperature-f'>
-          <div className='weater-temperature-max'>{ forecast.temp_max_f.toFixed(0)}</div>
-          <div className='weater-temperature-min'>{forecast.temp_min_f.toFixed(0)}</div>
+ 
+    <div>
+      <FontAwesomeIcon className='weater-icon' icon={forecast.icon} size="3x" />
+      <div class="weater-description">{forecast.description}</div>
+    </div>
+    <div className='weater-temperatures'>
+      <div className='weater-temperature'>
+        <div className='weater-temperature-max'><span className='weater-temperature-label'>H:</span>{temperatureUnit === 'f' ? forecast.temp_max_f.toFixed(0) : forecast.temp_max.toFixed(0)}</div>
+        <div className='weater-temperature-min'><span className='weater-temperature-label'>L:</span>{temperatureUnit === 'f' ? forecast.temp_min_f.toFixed(0) : forecast.temp_min.toFixed(0)}</div>
+        <div className='weater-temperature-unit'>
+          <span className={`weater-temperature-unit-switch ${temperatureUnit === "f" ? "selected" : ""}`} onClick={() => changeUnit('f')}>°F</span>
+          <span className={`weater-temperature-unit-switch ${temperatureUnit !== "f" ? "selected" : ""}`} onClick={() => changeUnit('c')}>°C</span >
         </div>
       </div>
-
+    </div>
     </div>
     <div className='weater-title'>
       {forecast.title} 
