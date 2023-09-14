0323141279a2b4a1e259c8ebcf5563a208d526bb
Mladen Milankovic <developer@mmlado.com>
2023-09-14 18:06:46 +0200

    Add look

--- src/components/Namedays/index.jsx
+++ src/components/Namedays/index.jsx
@@ -1,5 +1,7 @@
 import chech from './chech.json';
 
+import './namedays.scss';
+
 const chech_lookup = (date) => {
     return chech.find((element) => element.date > date).name;
 };
@@ -17,10 +19,10 @@ const Namedays = () => {
         'Chech': dates.map(date => chech_lookup(date))
     };
 
-    const items = Object.keys(names).map((key, index) => <div key={index}><div>{key}</div>{names[key].map((value, idx) => <div key={idx}>{value}</div>)}</div>)
+    const items = Object.keys(names).map((key, index) => <div key={index} className='row'><div>{key}</div>{names[key].map((value, idx) => <div key={idx}>{value}</div>)}</div>)
     return <div id='namedays'>
         <div className='title'>Namedays</div>
-        <div>
+        <div className='row header'>
             <div></div>
             <div>Today</div>
             <div>Tomorrow</div>
