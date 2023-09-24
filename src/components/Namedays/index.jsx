bddb28eafbb5ec88632e3de9209cb8ab8844f71d
Mladen Milankovic <developer@mmlado.com>
2023-09-24 16:15:01 +0200

    Add hungarian namedays.

--- src/components/Namedays/index.jsx
+++ src/components/Namedays/index.jsx
@@ -1,25 +1,35 @@
 import czech from './czech.json';
+import { today, tomorrow } from 'nevnap';
 
 import './namedays.scss';
 
 const czech_lookup = (date) => {
-    return czech.find((element) => element.date > date).name;
+    return czech.find((element) => element?.date > date)?.name;
 };
 
 const Namedays = () => {
-    const today = new Date();
-    const tomorrow = new Date(today);
-    tomorrow.setDate(today.getDate() + 1);
+    const today_date = new Date();
+    const tomorrow_date = new Date(today_date);
+    tomorrow_date.setDate(today_date.getDate() + 1);
     const dates = [
-        today.toISOString().slice(0, 10),
-        tomorrow.toISOString().slice(0, 10)
+        today_date.toISOString().slice(0, 10),
+        tomorrow_date.toISOString().slice(0, 10)
     ]; 
     
     const names = {
-        'Czech': dates.map(date => czech_lookup(date))
+        'Czech': dates.map(date => czech_lookup(date)),
+        'Hungarian': [
+            today().join(', '),
+            tomorrow().join(', '),
+        ]
     };
 
-    const items = Object.keys(names).map((key, index) => <div key={index} className='namedays-row'><div>{key}</div>{names[key].map((value, idx) => <div key={idx}>{value}</div>)}</div>)
+    const items = Object.keys(names).map((key, index) => {
+        return <div key={index} className='namedays-row'>
+            <div>{key}</div>
+            {names[key].map((value, idx) => <div key={idx}>{value}</div>)}
+        </div>
+    });
     return <div id='namedays'>
         <div className='main-area-title'>Namedays</div>
         <div className='namedays-wrapper'>
