280034c20407f038c3653a6c47cf2cde52ba563d
Mladen Milankovic <developer@mmlado.com>
2023-10-26 20:24:23 +0200

    Compensate for leap year in hungarian namedays.

--- src/components/Namedays/index.jsx
+++ src/components/Namedays/index.jsx
@@ -3,7 +3,23 @@ import nameday from './nameday.json';
 
 import './namedays.scss';
 
+function isLeapYear(year = new Date()) {
+	if (!(year instanceof Date) && typeof year !== 'number') {
+		throw new TypeError(`Expected \`year\` to be of type \`Date\` or \`number\`, got \`${typeof year}\``);
+	}
+
+	year = year instanceof Date ? year.getFullYear() : year;
+	return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
 const Namedays = () => {
+  if (isLeapYear(2024) && nameday[2][24]['hu'] == "Mátyás") {
+    for (let i = 28; i >= 23; --i) {
+      nameday[2][i+1]['hu'] = nameday[2][i]['hu'];
+    }
+    nameday[2][24]['hu'] = "";
+  }
+
   const today = new Date();
   const tomorrow = new Date(today);
   tomorrow.setDate(today.getDate() + 1);
